aa3af6506d6628711fa185954692212710848b8f
Daniel <d.rudnik@vp.pl>
2023-06-09 10:51:39 +0200

    add_tests_timeouts

--- utils/settings.py
+++ utils/settings.py
@@ -3,13 +3,23 @@ from pathlib import Path
 SCHEME = "http"
 HOST = "demostore.supersqa.com"
 URL = f"{SCHEME}://{HOST}"
-USER_LOGIN = f'login' #wstaw w razie potrzeby
-USER_PASSWORD = f'password' #wstaw w razie potrzeby
+USER_LOGIN = f'login'
+USER_PASSWORD = f'password'
+
+#Tests timeouts (seconds)
+
+SYSTEM_DELAY = 1
+RENDER_PAGE_DELAY = 2
+SERVER_DELAY = 5
+LOAD_DATA_DELAY = 10
+GET_DATA_FROM_SERVER_DELAY = 15
 
 # Run tests in headless mode
 HEADLESS = False
 WINDOW_SIZE = "1920,1080"
 
 #View tests reports
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_REPORTS_DIR = Path(__file__).resolve().parent.parent
+ALLURE_REPORTS_PATH = f'--alluredir={BASE_REPORTS_DIR}/reports'
+ALLURE_MAKE_REPORT = f'allure serve {BASE_REPORTS_DIR}/reports'
 
