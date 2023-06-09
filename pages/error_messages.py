c04da4bf620f97ed0b20b97966546df357b87cc8
Daniel <d.rudnik@vp.pl>
2023-06-09 10:57:31 +0200

    add_toast_and_error_messages_templates

---
+++ pages/error_messages.py
@@ -0,0 +1,13 @@
+LOGIN_ERROR = 'Invalid username or password.'
+
+ADD_FILE_ERROR = "Dodanie pliku jest wymagane"
+
+INTERNAL_SERVER_ERROR = ('Wystąpił błąd!\n'
+                        'Internal Server Error')
+
+INVALID_FILE_FORMAT_ERROR = 'nie jest w formacie json'
+
+UNPROCESSABLE_ENTITY = ('Wystąpił błąd!\n'
+                         'Kod błędu: 422 - Unprocessable Entity')
+
+
---
+++ pages/toast_messages.py
@@ -0,0 +1,5 @@
+TOAST_BOX_STATUS = 'Sukces'
+TOAST_BOX_DESCRIPTION = 'Zapisano użytkownika'
+
+TOAST_BOX_ADD_TO_CART = 'Dodano do koszyka'
+
