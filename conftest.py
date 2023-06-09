342d1a107662ccdc37863704da60bb9ad9eb2f90
Daniel <d.rudnik@vp.pl>
2023-06-09 10:33:16 +0200

    add_config_file

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+from utils.driver_factory import DriverFactory
+from utils import settings
+
+
+@pytest.fixture(scope="class")
+def driver(request):
+    driver = DriverFactory.get_driver('firefox')
+    driver.get(settings.URL)
+    request.cls.login_page = LoginPage(driver)
+    request.cls.main_page = MainPage(driver)
+    #tu dodajesz kolejne pages
+    yield driver
+    driver.quit()
+    time.sleep(2)
